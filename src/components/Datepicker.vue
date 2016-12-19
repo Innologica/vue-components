da68922033d9f792bd6ff2458d0a379daca8245e
Nikola Kostadinov <nikolakk@gmail.com>
2016-12-19 21:06:12 +0200

    lock to version Vue 2.1.6 because if different version is loaded is not working (e.g. 2.1.4)

--- src/components/Datepicker.vue
+++ src/components/Datepicker.vue
@@ -2,23 +2,23 @@
     <input :value="value" type="text"/>
 </template>
 
-<script>
-//    import defaults from '../defaults'
+<script type="">
+    import defaults from '../defaults'
 
     export default {
         props: ['options', 'value'],
         mounted: function () {
             var vm = this
             var el = $(this.$el)
-//            var options = $.extend(defaults.datepicker, this.options)
+            var options = Object.assign({}, defaults.datepicker, this.options)
 
             // init datepicker
-            el.datepicker(this.options)
-            .datepicker('setDate', this.value)
-            // emit event on change.
-            .on('change', function () {
-                vm.$emit('input', this.value)
-            })
+            el.datepicker(options)
+                .datepicker('setDate', this.value)
+                // emit event on change.
+                .on('change', function () {
+                    vm.$emit('input', this.value)
+                })
         },
         watch: {
             value: function (value) {
